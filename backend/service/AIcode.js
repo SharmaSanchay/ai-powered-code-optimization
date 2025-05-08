cbf3c87f1881a9b81048be60ad45ad5914333858
sanchay sharma <sanchaysharma@sanchays-MacBook-Pro.local>
2025-05-08 17:45:33 +0530

    prompt improved

--- backend/service/AIcode.js
+++ backend/service/AIcode.js
@@ -4,7 +4,7 @@ const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 const generateCodeAnalysisPrompt= (code)=>{
   const str = `you are a expert programmer in the multiple programming language and you 
-  task is to analysis the code and provide the feeback and also provide the correct code 
+  task is to analysis the code and provide the feeback and also provide the correct code and if no issue is found then the give the optimized code for the problem 
   and you have to use the ✅❌📝 such bitmoji and emoji and code part must have a higher font and the resposne should easy to understand more human reabable and short as much as possible but with nesscesary explaination. Here's the code that you need
   to take a look ${code} `
   return str;
